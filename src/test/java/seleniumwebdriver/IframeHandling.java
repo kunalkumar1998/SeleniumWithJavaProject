577f88194c5136ef2f79b4b392dd3ee7bb9d6297
kunal kumar <kunalkumar20898@gmail.com>
2025-10-25 18:11:58 +0530

    Iframe Concept

---
+++ src/test/java/seleniumwebdriver/IframeHandling.java
@@ -0,0 +1,41 @@
+package seleniumwebdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IframeHandling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Webdriver
+		WebDriver driver = new ChromeDriver();
+		//maximize
+		driver.manage().window().maximize();
+		
+		driver.get("https://ui.vision/demo/webtest/frames/");
+		
+		//frame 
+		WebElement frame =driver.findElement(By.xpath("//frame[@src='frame_3.html']"));
+		
+		driver.switchTo().frame(frame);
+		driver.findElement(By.cssSelector("input[name=\"mytext3\"]")).sendKeys("Kunal Kumar");
+        driver.switchTo().frame(0);    
+		
+		driver.findElement(By.xpath("//*[@id='i9']//div[@class='AB7Lab Id5V1']")).click();
+		
+		driver.switchTo().defaultContent();
+		
+		//frame 
+		WebElement frame1 =driver.findElement(By.xpath("//frame[@src='frame_2.html']"));
+		
+		driver.switchTo().frame(frame1);
+		driver.findElement(By.cssSelector("input[name=\"mytext2\"]")).sendKeys("Kunal Kumar");
+		
+		driver.switchTo().defaultContent();
+		
+		driver.close();		
+	}
+
+}
